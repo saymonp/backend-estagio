91fbb45ab9a774e6eb016c2ded5278ef4e84bcaf
saymonp <saymon.t@outlook.com>
2021-04-27 21:32:04 -0300

    Integração com S3

--- backend/services/s3.py
+++ backend/services/s3.py
@@ -3,35 +3,52 @@ import boto3
 from botocore.exceptions import ClientError
 import uuid
 
-s3_client = boto3.client('s3')
+from ..config.s3config import s3config
+from backend.errors import AppError
+
+s3_client = boto3.client('s3',
+                         aws_access_key_id=s3config.AWS_SERVER_PUBLIC_KEY,
+                         aws_secret_access_key=s3config.AWS_SERVER_SECRET_KEY,
+                         region_name=s3config.REGION_NAME
+                         )
+
 
 class S3(object):
-    self.allowed_mimes = [
-      "image/jpeg",
-      "image/pjpeg",
-      "image/png",
-      "3d??"
-    ]
 
-    def __init__(self, bucket_name, size_limits):
+    allowed_mimes = (
+        "jpeg",
+        "pjpeg",
+        "jpg",
+        "png",
+        "stl", "obj", "fbx", "collada", "3ds", "iges", "step", "vrml", "x3d")
+
+    def __init__(self, bucket_name, region, size_limits):
         self.bucket = bucket_name
         self.limits = size_limits
-        
+        self.region = region
 
-    def upload(self, file_name, object_name):
-        object_name = uuid.uuid4()
+    def upload(self, file, object_name):
+        if object_name.lower().endswith(self.allowed_mimes) == True:
+            pass
+        else:
+            raise AppError("Invalid data").set_code(404)
+
+        key = str(uuid.uuid4()) + object_name
+
         try:
-            response = s3_client.upload_file(file_name, self.bucket, object_name)
+            response = s3_client.upload_fileobj(
+                file, self.bucket, key, ExtraArgs={'ACL': 'public-read'})
             url = f"https://{self.bucket}.s3.{self.region}.amazonaws.com/{key}"
         except ClientError as e:
             logging.error(e)
             return False
-        return {"image_url": url, "key": key}
+        return {"file_url": url, "key": key}
 
     def delete(self, key):
-        s3_client.delete(self.bucket, key)
+        s3_client.delete_object(Bucket=self.bucket, Key=key)
+        return {"deleted": key}
 
     def get_url(self, key):
         url = f"https://{self.bucket}.s3.{self.region}.amazonaws.com/{key}"
-        
-        return {"image_url": url, "key": key}
+
+        return {"file_url": url, "key": key}
--- test/s3_test.py
+++ test/s3_test.py
@@ -1,12 +1,43 @@
 import pytest
 
 from backend.services.s3 import S3
-from backend.config import s3config
+from backend.config.s3config import s3config
 
 
-def test_user_registration():
-    s3 = S3(s3config.bucket_name, s3config.limits_file_size)
+def test_upload_file():
+    s3 = S3(s3config.buckets.upload_bucket,
+            s3config.REGION_NAME, s3config.limits_file_size)
 
-    
+    with open(r'C:\Users\saymo\Desktop\au.jpg', 'rb') as data:
+        response = s3.upload(data, "au.jpg")
 
-    assert response == {"msg": "Verification email sent"}
+        print(response)
+    assert True == True
+
+
+def test_delete_file():
+    s3 = S3(s3config.buckets.upload_bucket,
+            s3config.REGION_NAME, s3config.limits_file_size)
+
+    with open(r'C:\Users\saymo\Desktop\au.jpg', 'rb') as data:
+        response = s3.upload(data, "au.jpg")
+
+    print(response)
+    s3.delete(response["key"])
+
+    print(response)
+    assert True == True
+
+
+def test_get_url():
+    s3 = S3(s3config.buckets.upload_bucket,
+            s3config.REGION_NAME, s3config.limits_file_size)
+
+    with open(r'C:\Users\saymo\Desktop\au.jpg', 'rb') as data:
+        response = s3.upload(data, "au.jpg")
+
+    response = s3.get_url(response["key"])
+
+    print(response)
+
+    assert response["file_url"] is not None
